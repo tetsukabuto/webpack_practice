a77b65347687a956bc23f5d112ab74604a911ff1
Tatsuo Sato <29546246+tetsukabuto@users.noreply.github.com>
2020-05-31 01:48:31 +0900

    Improve file structure

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,9 @@
 //export defaultで外部出力可能にした./modules/my.jsを読み込む
-import my from "./modules/my.js";
+import my from "./my.js";
 
 // npm install --save-dev css-loader@3.4.2でパッケージをダウンロードして設定ファイルにloaderを指定した後./modules/my.cssをモジュールとして読み込む
 // cssのスタイルを反映させるにはstyle-loaderも必要になる
-import "./modules/my.css";
+import "../stylesheets/main.css";
 
 console.log("webpack");
 my();
--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,22 @@ const path = require("path");
 //pluginのmini-css-extract-pluginを読み込む関数
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-//pluginのhtml-webpack-pluginを読み込む関数※HtmlWebpackPluginはdistにpluginで設定したindex.htmlを出力するplugin(出力されたhtmlにはconfigで設定したmain.jsやmain.cssも読み込んでくれる)
+//pluginのhtml-webpack-pluginを読み込む関数※HtmlWebpackPluginはdistにpluginで設定したindex.htmlを出力するplugin(出力されたhtmlにはconfigで設定したsrcフォルダのjsやcssも読み込んでくれる)
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+//pluginのclean-webpack-pluginを読み込む関数※{ CleanWebpackPlugin }としているのはpluginの中にある様々なクラスからCleanWebpackPluginのクラスのみを読み込む設定にするため
+// CleanWebpackPluginはsrcフォルダの内容をdistフォルダに出力する際、クリーンアップしてくれるplugin※ゴミファイルの混入を防ぐ
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   //entryに エントリーポイント(webpack実行時、いちばん最初に読み込まれる)のindex.jsを指定
-  entry: "./src/index.js",
+  entry: "./src/javascripts/main.js",
   //outputに webpackで処理されたファイルの出力される内容の設定を指定
   output: {
     // 出力先のフォルダを指定。絶対パスで出力先を記述しないとエラーになるconst pathで絶対パスを取得する(パラメータの第一引数の__dirnameは現在のプロジェクトを指す、第二引数には出力先のフォルダを指定)
     path: path.resolve(__dirname, "./dist"),
     // 出力されるファイル名を設定(デフォルトはmain.js)
-    filename: "main.js",
+    filename: "javascripts/main.js",
   },
   //インストールしたloaderの設定等を指定
   module: {
@@ -44,9 +48,14 @@ module.exports = {
   },
   // pluginを読み込みを行う
   plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
+    new MiniCssExtractPlugin({
+      // distに出力する際のファイル名を指定
+      filename: "./stylsheets/main.css",
     }),
+    new HtmlWebpackPlugin({
+      // srcからdistに出力する際のファイルを指定
+      template: "./src/templates/index.html",
+    }),
+    new CleanWebpackPlugin(),
   ],
 };
